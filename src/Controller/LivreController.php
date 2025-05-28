62934a699e80a4b2e3576d5c52eca4b8c6f1532f
haidou tounsia <haidou.tounsia@gmail.com>
2025-05-28 12:00:31 +0200

    afficher les livres de la bibliotheque

---
+++ src/Controller/LivreController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Livre;
+use App\Form\LivreForm;
+use App\Repository\LivreRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/livre')]
+final class LivreController extends AbstractController
+{
+    #[Route('/index', name: 'app_livre_index', methods: ['GET'])]
+    public function index(LivreRepository $livreRepository): Response
+    {
+        return $this->render('livre/index.html.twig', [
+            'livres' => $livreRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_livre_show', methods: ['GET'])]
+    public function show(Livre $livre): Response
+    {
+        return $this->render('livre/show.html.twig', [
+            'livre' => $livre,
+        ]);
+    }
+}
---
+++ src/Form/LivreForm.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Emprunt;
+use App\Entity\Livre;
+use App\Entity\Reservation;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LivreForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre')
+            ->add('auteur')
+            ->add('date_edition')
+            ->add('categorie')
+            ->add('disponibilite')
+            ->add('emprunt', EntityType::class, [
+                'class' => Emprunt::class,
+                'choice_label' => 'id',
+            ])
+            ->add('reservation', EntityType::class, [
+                'class' => Reservation::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Livre::class,
+        ]);
+    }
+}

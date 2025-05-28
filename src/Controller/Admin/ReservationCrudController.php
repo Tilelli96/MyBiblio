365e90c299c5ad3c1983348408b641566ebf3a09
haidou tounsia <haidou.tounsia@gmail.com>
2025-05-28 16:32:37 +0200

    ajouter le bouton pour valider une reservation

--- src/Controller/Admin/ReservationCrudController.php
+++ src/Controller/Admin/ReservationCrudController.php
@@ -3,12 +3,16 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Reservation;
+use App\Entity\Emprunt;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 
 class ReservationCrudController extends AbstractCrudController
 {
@@ -26,4 +30,14 @@ class ReservationCrudController extends AbstractCrudController
             DateField::new('date_reservation', 'Date de reservation')
         ];
     }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        $valider = Action::new('valider', 'Valider')
+        ->linkToCrudAction('validerReservation');
+
+    return $actions
+        ->add(Crud::PAGE_INDEX, $valider);
+    }
+
 }
